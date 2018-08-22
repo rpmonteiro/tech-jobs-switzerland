7d5dd00dd8787faacacd08b250d9b7096a56deec
Ric <ricpmonteiro@gmail.com>
2018-08-22 20:56:24 +0200

    Add dummy-data and initial DB migration

--- src/endpoints/jobs.ts
+++ src/endpoints/jobs.ts
@@ -1,5 +1,6 @@
 import { BaseContext } from 'koa';
+const jobsDummyData = require('../../dummy-data/jobs.json');
 
 export async function getJobs(ctx: BaseContext) {
-
+  ctx.body = jobsDummyData
 }
