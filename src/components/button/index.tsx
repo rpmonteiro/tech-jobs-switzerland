4254ae4cd178ed79cc0b2df023b2f10ae88deda5
Ric <ricpmonteiro@gmail.com>
2018-09-05 18:46:36 +0200

    Remove unused props/imports

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,17 +1,14 @@
 import * as preact from 'preact';
 import { Link } from '../link';
-import classnames from 'classnames';
 
 interface Props {
   to?: string;
   text: string;
-  orange: boolean;
-  containerClass?: string;
 }
 
 export class Button extends preact.Component<Props> {
   render() {
-    const { text, containerClass, to, orange } = this.props;
+    const { text, to } = this.props;
 
     const buttonText = (
       <span className="button__text">
