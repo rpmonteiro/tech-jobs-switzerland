0afde5de6f5acf4d5cd490d36ba8386cef136944
shylands <steve@shylands.com>
2019-01-13 22:12:12 +0100

    header styling improvements and adding header divider bar

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,6 +1,7 @@
 import * as preact from 'preact'
 import { Link } from '../link'
 import logoSvg from '../../assets/logo.svg'
+import navbarDividerSvg from '../../assets/navbar-divider.svg'
 import { Button } from '../button'
 import './styles.less'
 
@@ -11,6 +12,9 @@ export const NavBar = () => (
         <img src={logoSvg} />
       </Link>
       <Button className="navbar__button" text="Post a job" to="/post-a-job" />
+    </div>
+    <div className="navbar__divider  container-fluid">
+
     </div>
   </nav>
 )
